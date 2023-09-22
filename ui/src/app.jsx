0328e7349e0854e87ba313b15675b205acd6f32f
Bit <69608518+Ex4byt3@users.noreply.github.com>
2023-09-22 20:59:26 +0000

    test push on app.jsx

--- ui/src/app.jsx
+++ ui/src/app.jsx
@@ -54,6 +54,28 @@ export function App() {
   }, [loadFiles, s3, client]);
 
   return (
-    <main className="flex items-center justify-center min-h-screen"></main>
+     
+ <main className="flex items-center justify-center min-h-screen">
+ <div className="max-w-md space-y-6 py-20">
+   <h1 className="text-3xl font-bold">Welcome to Onyx, Anthony</h1>
+   <p>Lets get started Team 10!</p>
+   <p>Here&apos;s your urbit&apos;s installed apps:</p>
+   {apps && (
+     <ul className="space-y-4">
+       {Object.entries(apps).map(([desk, app]) => (
+         <li key={desk} className="flex items-center space-x-3 text-sm leading-tight">
+           <AppTile {...app} />
+           <div className="flex-1 text-black">
+             <p>
+               <strong>{app.title || desk}</strong>
+             </p>
+             {app.info && <p>{app.info}</p>}
+           </div>
+         </li>
+       ))}
+     </ul>
+   )}
+ </div>
+</main>
   );
 }
