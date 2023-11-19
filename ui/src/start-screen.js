9b482be462cf86f83759a55e0dd1fc13fb0c18f0
ihatejordan <jordan.uhrmann@gmail.com>
2023-11-19 16:41:08 -0500

    Formatting and added empty file buttons

--- ui/src/start-screen.js
+++ ui/src/start-screen.js
@@ -5,10 +5,21 @@ function Start({ onClickNewFolder }) {
   return (
     <div className="start-screen">
       <div className="inner-start-screen">
-        <h1>No File Open</h1>
+        <h1>No File is Open</h1>
+        <div class="button-container">
         <button className="newFolderButton" onClick={onClickNewFolder}>
-          Create a new Folder
-        </button>
+            Create a new File
+          </button>
+          <button className="gotoFileButton" onClick={onClickNewFolder}>
+            Go to File
+          </button>
+          <button className="recentButton" onClick={onClickNewFolder}>
+            Recent
+          </button>
+          <button className="closeButton" onClick={onClickNewFolder}>
+            Close
+          </button>
+        </div>
       </div>
     </div>
   );
