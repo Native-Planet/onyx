b540d240a6a34a57d7d5f02636adbacfa479e08e
Grafton-LeGare <grafton.legare355@knights.ucf.edu>
2023-11-22 00:39:45 -0500

    Added tree view, awaiting functionality

--- ui/src/management/treeView.js
+++ ui/src/management/treeView.js
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import { Tree } from "react-arborist";
+import { sample_data } from "./sample_data.js";
+import Node from "./Node";
 import "../styles.css";
 
 
@@ -9,8 +12,15 @@ function App(){
     return (
         
         <div className="treeView-management">
-            
-            
+            <Tree className="main-tree" 
+                initialData={sample_data} 
+                height={500}
+                indent={15}
+                rowHeight={35}
+                openByDefault={false}
+            >
+                {Node}
+            </Tree>
         </div>
     );
 }
